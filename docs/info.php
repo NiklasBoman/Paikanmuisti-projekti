60f03e6c46e7539bc5aaa08ca4efe581486b746a
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2026-02-17 13:13:08 +0200

    Add info.php file with basic HTML structure

---
+++ docs/info.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<html lang="fi">
+<head>
+<meta charset="UTF-8">
+<title>Info</title>
+<link rel="stylesheet" href="Tyyli.css">
+</head>
+<nav>
+  <h2>Paikan Muisti</h2>
+  <ul>
+    <li><a href="kartta.php">Kartta</a></li>
+    <li><a href="tarinat.php">Tarinat</a></li>
+    <li><a href="info.php">Info</a></li>
+  </ul>
+</nav>
+</body>
+</html>
