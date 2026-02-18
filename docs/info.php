23d52f7c87fa3fe9eb91a99333de54163f20fa47
NiklasBoman <nikke.boman@gmail.com>
2026-02-18 14:14:21 +0200

    Update info.php

--- docs/info.php
+++ docs/info.php
@@ -10,7 +10,8 @@
   <h2>Paikan Muisti</h2>
   <ul>
     <li><a href="kartta.php">Kartta</a></li>
-    <li><a href="tarinat.php">Tarinat</a></li>
+    <li><a href="tarinat.php">Arkisto
+    </a></li>
     <li><a href="info.php">Info</a></li>
   </ul>
 </nav>
