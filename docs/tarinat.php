baecfc7d3ae56a9554e0bbfeee418456da1575db
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2026-02-18 11:28:28 +0200

    Refactor tarinat.php for improved functionality

--- docs/tarinat.php
+++ docs/tarinat.php
@@ -1,20 +1,56 @@
 <?php
-$json = file_get_contents("tarinat.json");
-$tarinat = json_decode($json, true);
+// 1. LUE TARINAT VANHALTA SIVULTA
+$html = file_get_contents("ABCD_t.php");
 
+// 2. Muunna merkistö (poistaa □-merkit)
+$html = mb_convert_encoding($html, 'UTF-8', 'ISO-8859-1');
+
+// 3. Etsi kaikki <div id="...">...</div>
+preg_match_all('/<div id=[\'"]([^\'"]+)[\'"][^>]*>(.*?)<\/div>/si', $html, $matches);
+
+$tarinat = [];
+
+// 4. Käy läpi jokainen div
+for ($i = 0; $i < count($matches[1]); $i++) {
+
+    $id = $matches[1][$i];
+    $content = trim($matches[2][$i]);
+
+    // Poista ohjausmerkit
+    $content = preg_replace('/[\x00-\x1F\x7F]/u', '', $content);
+
+    // 5. Jaa tarinat "Paikka:" -kohdan perusteella
+    $parts = preg_split('/(?=<b>Paikka)/i', $content, -1, PREG_SPLIT_NO_EMPTY);
+
+    foreach ($parts as $index => $part) {
+
+        // Lisää rivinvaihdot näkyviin
+        $part = str_replace("\n", "<br>", $part);
+
+        $tarinat[] = [
+            "id" => $id . "_" . $index,   // uniikki ID
+            "kuvaus" => $part
+        ];
+    }
+}
+
+// 6. POISTA TYHJÄT TARINAT
+$tarinat = array_filter($tarinat, function($t) {
+    return trim(strip_tags($t["kuvaus"])) !== "";
+});
+
+// 7. HAKU
 $hakusana = isset($_GET['q']) ? strtolower($_GET['q']) : '';
-
 if ($hakusana) {
-    $tarinat = array_filter($tarinat, function($tarina) use ($hakusana) {
-        return strpos(strtolower($tarina["otsikko"]), $hakusana) !== false ||
-               strpos(strtolower($tarina["kuvaus"]), $hakusana) !== false;
+    $tarinat = array_filter($tarinat, function($t) use ($hakusana) {
+        return strpos(strtolower($t["kuvaus"]), $hakusana) !== false;
     });
 }
-//Näyttää 5 sattunnaista tarinaa
+
+// 8. 5 SATUNNAISTA
 $tarinat = array_values($tarinat);
-shuffle($tarinat); $tarinat = array_slice($tarinat, 0, 5);
-
-
+shuffle($tarinat);
+$tarinat = array_slice($tarinat, 0, 5);
 ?>
 <!DOCTYPE html>
 <html lang="fi">
@@ -33,8 +69,7 @@ shuffle($tarinat); $tarinat = array_slice($tarinat, 0, 5);
     <li><a href="info.php">Info</a></li>
   </ul>
 </nav>
-<form method="GET" class="haku">
-  
+
 <section class="tarinat">
   <h2>Tarinat</h2>
 
@@ -47,8 +82,8 @@ shuffle($tarinat); $tarinat = array_slice($tarinat, 0, 5);
   <div class="tarina-list">
     <?php foreach ($tarinat as $tarina): ?>
       <div class="tarina">
-        <h3><?php echo $tarina["otsikko"]; ?></h3>
-        <p><?php echo $tarina["kuvaus"]; ?></p>
+        <?php echo $tarina["kuvaus"]; ?>
+        <br>
         <a href="kartta.php?id=<?php echo $tarina["id"]; ?>">Näytä kartalla</a>
       </div>
     <?php endforeach; ?>
