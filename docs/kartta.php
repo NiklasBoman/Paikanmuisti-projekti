3fd1187005c50be6c5657a0f71da861b78d5304d
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2026-02-18 13:46:21 +0200

    Update kartta.php

--- docs/kartta.php
+++ docs/kartta.php
@@ -1,40 +1,15 @@
 <?php
-// Hae id URL:sta
-$id = $_GET['id'] ?? null;
+$lat = 62.95556;
+$lng = 26.75556;
+$zoom = 12;
 
-// Lue JSON
-$json = file_get_contents("tarinat.json");
-$tarinat = json_decode($json, true);
-
-// Etsi oikea tarina
-$tarina = null;
-foreach ($tarinat as $t) {
-    if ($t["id"] == $id) {
-        $tarina = $t;
-        break;
-    }
-}
-
-if (!$tarina) {
-    $tarina = [
-        "otsikko" => "",
-        "kuvaus" => "",
-        "lat" => 62.95646895043363,
-        "lng" => 26.739824232731905
-    ];
-}
-
-
-//Luo Google Maps iframe URL koordinaateilla
-$lat = $tarina["lat"];
-$lng = $tarina["lng"];
-$mapUrl = "https://www.google.com/maps?q={$lat},{$lng}&z=12&output=embed";
+$mapUrl = "https://www.google.com/maps?q={$lat},{$lng}&z={$zoom}&output=embed";
 ?>
 <!DOCTYPE html>
 <html lang="fi">
 <head>
 <meta charset="UTF-8">
-<title><?php echo $tarina["otsikko"]; ?></title>
+<title>Tervon kartta</title>
 <link rel="stylesheet" href="Tyyli.css">
 </head>
 <body>
@@ -48,20 +23,9 @@ $mapUrl = "https://www.google.com/maps?q={$lat},{$lng}&z=12&output=embed";
   </ul>
 </nav>
 
-<!-- Tarinakortti -->
-<?php if ($tarina["otsikko"] !== ""): ?>
-<div class="tarina-card">
-  <h3><?php echo $tarina["otsikko"]; ?></h3>
-  <p><?php echo $tarina["kuvaus"]; ?></p>
-</div>
-<?php endif; ?>
-
-
-<!-- Google Maps iframe tarinan koordinaateilla -->
 <iframe 
     src="<?php echo $mapUrl; ?>"
     class="map"
-    style="border:0;"
     allowfullscreen=""
     loading="lazy"
     referrerpolicy="no-referrer-when-downgrade">
