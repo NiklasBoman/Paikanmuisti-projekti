77bdf5f9ba6024d4c156c2157667ed2bcbd67863
NiklasBoman <nikke.boman@gmail.com>
2026-02-25 10:32:56 +0200

    Update info.php with new content and formatting

--- docs/info.php
+++ docs/info.php
@@ -1,27 +1,46 @@
 <!DOCTYPE html>
-<html>
 <html lang="fi">
 <head>
 <meta charset="UTF-8">
 <title>Info</title>
-<link rel="stylesheet" href="Tyyli.css">
+<link rel="stylesheet" href="tyyli.css">
 </head>
+
+<body>
+
 <nav>
   <h2>Paikan Muisti</h2>
   <ul>
     <li><a href="kartta.php">Kartta</a></li>
-    <li><a href="tarinat.php">Arkisto
-    </a></li>
+    <li><a href="tarinat.php">Arkisto</a></li>
     <li><a href="info.php">Info</a></li>
   </ul>
 </nav>
+
+<div class="info">
+  <h1>Info</h1>
+
+  <h3>Paikan muisti</h3>
+  <p>
+    Tervossa on lukematon määrä paikannimiä. Nimet kertovat metsästys-,
+    kalastus- ja pyyntikulttuurista ja kaskiviljelystä – tervolaisesta
+    menneisyydestä ja nykyisyydestä. Nimet ovat kielen ja historian
+    hyperlinkkejä. Niiden kautta avautuu paikan muisti.
+  </p>
+
+  <p>
+    Paikan muisti –osion nimistö perustuu Kotimaisten kielten
+    tutkimuskeskuksen paikannimiarkiston Tervosta koottuihin nimitietoihin.
+  </p>
+
+  <h3>Tietoja sivusta</h3>
+  <p>
+    Sivulla voi hakea tarinoita paikan nimillä. Tarinat on koottu Tervon
+    paikannimiarkiston tietokannasta. Tekstit on kirjoitettu suoraan
+    tietokantaan, joten niissä saattaa olla kirjoitusvirheitä tai
+    vanhentunutta tietoa.
+  </p>
+</div>
+
 </body>
-<h1>Info</h1>
-<h3>Paikan muisti</h3>
-<p>Tervossa on lukematon määrä paikannimiä. Nimet kertovat metsästys- kalastus- ja pyyntikulttuurista ja kaskiviljelystä - tervolaisesta menneisyydestä ja nykyisyydestä. 
-Nimet ovat kielen ja historian hyperlinkkejä. Niiden kautta avautuu paikan muisti. 
-Paikan muisti –osion nimistö perustuu Kotimaisten kielten tutkimuskeskuksen paikannimiarkiston Tervosta koottuihin nimitietoihin<p>
-<br>
-<h3>Tietoja sivusta</h3>
-<p>Sivulla voi hakea tarinoita paikan nimillä. Tarinat on koottu Tervon paikannimiarkiston tietokannasta. Tarinoiden tekstit on kirjoitettu suoraan tietokantaan, joten niissä saattaa olla kirjoitusvirheitä tai vanhentunutta tietoa.
-</html>
+</html> 
